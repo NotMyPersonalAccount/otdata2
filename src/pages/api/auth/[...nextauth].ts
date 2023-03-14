e3642fdcd39b6aa2e33626aa98e8bae0fc03d08e
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-14 13:56:27 -0700

    Embed information in the JWT

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,37 @@
-import NextAuth from "next-auth";
+import User from "@/models/User";
+import { connectMongo } from "@/utils/mongoose";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
 	providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
-	]
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+		})
+	],
+	callbacks: {
+		async jwt({ token, user, account, profile, isNewUser }) {
+			if (user?.email === undefined) {
+				//TODO: Handle this properly
+				return token;
+			}
+			await connectMongo();
+			const dbUser = await User.findOne({ otemail: user.email });
+			if (dbUser === null) {
+				//TODO: Handle this properly
+				return token;
+			}
+			token.otdata = {
+				admin: dbUser.isadmin,
+				role: dbUser.role
+			};
+			return token;
+		},
+		async session({ session, user, token }) {
+			return { ...session, ...token.otdata };
+		}
+	}
 };
 export default NextAuth(authOptions);
+
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+
+type ExtraSessionInfo = {
+	admin: boolean;
+	role: string;
+};
+
+declare module "next-auth" {
+	interface Session extends DefaultSession, ExtraSessionInfo {}
+}
+
+declare module "next-auth/jwt" {
+	interface JWT extends DefaultJWT {
+		otdata: ExtraSessionInfo;
+	}
+}
+
