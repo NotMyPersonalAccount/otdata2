9d4e19b717dc04eb514f93fec948bba1b982d1d2
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-28 13:24:54 -0700

    Add `getClassroomByGoogleId` function

--- src/lib/database/class.ts
+++ src/lib/database/class.ts
@@ -1,7 +1,26 @@
-import { GEnrollment } from '@prisma/client';
-import prisma from '.';
+import { GEnrollment, Prisma } from "@prisma/client";
+import prisma from ".";
 
-export async function getEnrollmentByUser(userId: string): Promise<GEnrollment[]> {
+export async function getClassroomByGoogleId<
+	T extends
+		| Prisma.GoogleClassroomFindUniqueArgs
+		| Omit<Prisma.GoogleClassroomFindUniqueArgs, "where">
+>(
+	id: string,
+	findArgs: T = {} as T
+): Promise<Prisma.GoogleClassroomGetPayload<T> | null> {
+	return (await prisma.googleClassroom.findUnique({
+		...findArgs,
+		where: {
+			...(findArgs as Prisma.GoogleClassroomFindUniqueArgs).where,
+			google_classroom_id: id
+		}
+	})) as Prisma.GoogleClassroomGetPayload<T>;
+}
+
+export async function getEnrollmentByUser(
+	userId: string
+): Promise<GEnrollment[]> {
 	return await prisma.gEnrollment.findMany({
 		where: {
 			owner_id: userId
