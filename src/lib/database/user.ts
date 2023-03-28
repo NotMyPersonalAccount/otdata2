f674b727c0f4aee624c8ba67daa53df78bf246f0
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-28 09:19:16 -0700

    Refactor user getters to be more powerful

--- src/lib/database/user.ts
+++ src/lib/database/user.ts
@@ -1,33 +1,22 @@
-import { User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import prisma from ".";
 
-export async function getUserByAeriesId(
-	aeriesId: number
-): Promise<User | null> {
-	return await prisma.user.findFirst({
-		where: {
-			aeries_id: aeriesId
-		}
-	});
+function getUserGetter<T extends keyof Prisma.UserWhereInput>(key: T) {
+	return async function <FindArgs extends Prisma.UserFindFirstArgs>(
+		value: Prisma.UserWhereInput[T],
+		findArgs: FindArgs = {} as FindArgs
+	): Promise<Prisma.UserGetPayload<FindArgs> | null> {
+		const args = findArgs;
+		if (!args.where) args.where = {};
+		args.where[key] = value;
+
+		return (await prisma.user.findFirst(
+			args
+		)) as Prisma.UserGetPayload<FindArgs>;
+	};
 }
 
-export async function getUserByGoogleId(
-	googleId: string
-): Promise<User | null> {
-	return await prisma.user.findFirst({
-		where: {
-			google_id: googleId
-		}
-	});
-}
-
-export async function getUserBySchoolEmail(
-	email: string
-): Promise<User | null> {
-	return await prisma.user.findFirst({
-		where: {
-			ot_email: email
-		}
-	});
-}
+export const getUserByAeriesId = getUserGetter("aeries_id");
+export const getUserByGoogleId = getUserGetter("google_id");
+export const getUserBySchoolEmail = getUserGetter("ot_email");
 
