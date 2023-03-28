fcc022ceb277109c2153378f4dab69ff621f802c
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-28 13:25:08 -0700

    Add dayjs relativeTime plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+
+dayjs.extend(relativeTime);
 
 export default function App({
 	Component,
