eccb3c21adb6c5b00c9fb911b0240c602e8227c9
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-28 21:45:34 -0700

    Add `createCheckin` function

---
+++ src/lib/database/checkin.ts
@@ -0,0 +1,23 @@
+import { GoogleClassroom, Prisma, User } from "@prisma/client";
+
+export async function createCheckin(
+	classroom: GoogleClassroom,
+	user: User,
+	description: string,
+	status: string,
+	working_on: string
+) {
+	return await prisma?.checkin.create({
+		data: {
+			google_classroom_id: classroom.google_classroom_id,
+			description,
+			status,
+			working_on,
+			student_id: user.id,
+			google_classroom_name: (classroom.class_dict as Prisma.JsonObject)
+				.name as string,
+			classroom_id: classroom.id
+		}
+	});
+}
+
