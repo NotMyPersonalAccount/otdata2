cad24581f1c33b512de880ada355460ba12b32ef
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-03-29 22:00:00 -0700

    Add `getCheckin`/`deleteCheckin`/`getLastCheckin` functions

--- src/lib/database/checkin.ts
+++ src/lib/database/checkin.ts
@@ -23,6 +23,33 @@ export async function createCheckin(
 	});
 }
 
+export async function getCheckin(id: string) {
+	return await prisma.checkin.findUnique({
+		where: {
+			id
+		}
+	});
+}
+
+export async function deleteCheckin(id: string) {
+	return await prisma.checkin.delete({
+		where: {
+			id
+		}
+	});
+}
+
+export async function getLastCheckin(
+	googleClassroomId: string,
+	userId: string
+) {
+	return await prisma.checkin.findFirst({
+		where: {
+			google_classroom_id: googleClassroomId,
+			student_id: userId
+		},
+		orderBy: {
+			create_date: "desc"
 		}
 	});
 }
