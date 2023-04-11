f65a07d8d92954e40e8c42718ba10ef101b798a8
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-04-11 00:11:50 -0700

    Enable Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import ImpersonationModal from "@/components/modals/Impersonation";
 import ImpersonationBanner from "@/components/ImpersonationBanner";
 
@@ -13,9 +14,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 	return (
 		<SessionProvider session={session}>
 			<Navbar />
-			<ImpersonationBanner/>
+			<ImpersonationBanner />
 			<ImpersonationModal />
 			{pageProps.error ? pageProps.error : <Component {...pageProps} />}
+			<Analytics />
 		</SessionProvider>
 	);
 }
