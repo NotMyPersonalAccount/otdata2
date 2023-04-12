4be9f07398a3f22ce0f558b4319a9c2f24cbc967
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-04-12 10:06:55 -0700

    Add student querying

--- src/components/forms/FindStudentForm.tsx
+++ src/components/forms/FindStudentForm.tsx
@@ -3,12 +3,36 @@ import { trpc } from "@/lib/api/trpc";
 import { User } from "@prisma/client";
 import Form from "./Form";
 import FormInput from "./FormInput";
+import FormMultiCheckbox from "./FormMultiCheckbox";
+import { useEffect, useState } from "react";
+import { inferRouterOutputs } from "@trpc/server";
+import { AppRouter } from "@/lib/api/routers/_app";
+import Spinner from "../Spinner";
 
 type Props = {
 	onFind?: (data: User[]) => void;
 };
 
+export const queryOptionsAvailable = {
+	aeries_gender: "Gender",
+	aeries_ethnicity: "Ethnicity",
+	cohort: "Cohort",
+	grade: "Grade"
+};
+
 export default function FindStudentForm({ onFind }: Props) {
+	const [queryEnabled, setQueryEnabled] = useState(false);
+	const [queryOptions, setQueryOptions] =
+		useState<inferRouterOutputs<AppRouter>["user"]["getQueryOptions"]>();
+	useEffect(() => {
+		if (queryEnabled && !queryOptions)
+			(async () => {
+				setQueryOptions(await trpc.user.getQueryOptions.query({}));
+			})();
+	}, [queryEnabled, queryOptions]);
+
+	const queryVisible = queryEnabled && queryOptions;
+
 	return (
 		<Form
 			onSubmit={async (data: FindUserInput) => {
@@ -19,6 +43,29 @@ export default function FindStudentForm({ onFind }: Props) {
 			<FormInput name="first_name" label="First Name" />
 			<FormInput name="last_name" label="Last Name" />
 			<FormInput name="aeries_id" label="Aeries ID" />
+			<div className="md:col-span-2 lg:col-span-3 xl:col-span-4 flex gap-2">
+				<span className="text-xl font-bold">Enable Advanced Query</span>
+				<input
+					type="checkbox"
+					onChange={e => setQueryEnabled(e.target.checked)}
+				/>
+			</div>
+			{queryVisible
+				? Object.entries(queryOptionsAvailable).map(([key, label]) => {
+						return (
+							<FormMultiCheckbox
+								key={key}
+								name={key}
+								label={label}
+								choices={queryOptions?.[key]! ?? []}
+							/>
+						);
+				  })
+				: queryEnabled && (
+						<div className="w-8 h-8">
+							<Spinner />
+						</div>
+				  )}
 		</Form>
 	);
 }
--- src/lib/api/routers/user.ts
+++ src/lib/api/routers/user.ts
@@ -1,20 +1,67 @@
 import prisma from "@/lib/database/prisma";
 import { TypeOf, z } from "zod";
 import { procedure, router } from "..";
+import { Prisma } from "@prisma/client";
+import { queryOptionsAvailable } from "@/components/forms/FindStudentForm";
 
 const findUserSchema = z.object({
 	first_name: z.string(),
 	last_name: z.string(),
-	aeries_id: z.string()
+	aeries_id: z.string(),
+	aeries_ethnicity: z.array(z.string()).optional(),
+	cohort: z.array(z.string()).optional(),
+	grade: z.array(z.number()).optional(),
+	aeries_gender: z.array(z.string()).optional()
 });
 export type FindUserInput = TypeOf<typeof findUserSchema>;
 
 export const userRouter = router({
+	getQueryOptions: procedure.input(z.object({})).query(async ({ ctx }) => {
+		if (!ctx.session) throw new Error("Not logged in");
+		if (!ctx.session.admin && !ctx.session.originalData)
+			throw new Error("Not authorized");
+
+		const options = {} as {
+			[key: string]: {
+				label: string;
+				value: string;
+			}[];
+		};
+		await Promise.all(
+			Object.keys(queryOptionsAvailable).map(async (key: string) => {
+				const field = key as Prisma.UserScalarFieldEnum;
+				options[field] = (
+					await prisma.user.findMany({
+						distinct: field,
+						orderBy: { [field]: "asc" }
+					})
+				)
+					.filter(user => user[field])
+					.map(user => {
+						return {
+							label: user[field] as string,
+							value: user[field] as string
+						};
+					});
+			})
+		);
+
+		return options;
+	}),
 	find: procedure.input(findUserSchema).query(async ({ input, ctx }) => {
-		const { first_name, last_name, aeries_id } = input;
+		const {
+			first_name,
+			last_name,
+			aeries_id,
+			aeries_ethnicity,
+			cohort,
+			grade,
+			aeries_gender
+		} = input;
 
 		if (!ctx.session) throw new Error("Not logged in");
-		if (!ctx.session.admin && !ctx.session.originalData) throw new Error("Not authorized");
+		if (!ctx.session.admin && !ctx.session.originalData)
+			throw new Error("Not authorized");
 
 		return await prisma.user.findMany({
 			where: {
@@ -26,7 +73,19 @@ export const userRouter = router({
 					contains: last_name,
 					mode: "insensitive"
 				},
-				aeries_id: aeries_id ? parseInt(aeries_id) : undefined
+				aeries_id: aeries_id ? parseInt(aeries_id) : undefined,
+				aeries_ethnicity: {
+					in: aeries_ethnicity
+				},
+				cohort: {
+					in: cohort
+				},
+				grade: {
+					in: grade
+				},
+				aeries_gender: {
+					in: aeries_gender
+				}
 			}
 		});
 	})
