4e3a3d85545bdda3a040f0cc7325798f4201aaaa
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-04-16 16:48:20 -0700

    Fix "Viewing as" banner always showing for non-admins

--- src/components/ImpersonationBanner.tsx
+++ src/components/ImpersonationBanner.tsx
@@ -4,8 +4,8 @@ import { useRouter } from "next/router";
 export default function ImpersonationBanner() {
 	const router = useRouter();
 	const { data: session, update } = useSession();
-	return session &&
-		session.originalData?.currUserId !== session.currUserId ? (
+	return session?.originalData &&
+		session.originalData.currUserId !== session.currUserId ? (
 		<div className="flex flex-wrap justify-between bg-red-500 text-white text-lg px-4 py-2">
 			<p>Viewing as {session.name}</p>
 			<button
