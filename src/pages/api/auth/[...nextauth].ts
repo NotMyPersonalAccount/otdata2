c4eb8994f0f07abeeb6840c1992500e690268ddb
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-04-16 16:53:28 -0700

    Create user if not found

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/database/prisma";
 import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
 	secret: process.env.NEXTAUTH_SECRET as string,
@@ -11,16 +11,30 @@ export const authOptions: AuthOptions = {
 		})
 	],
 	callbacks: {
-		async jwt({ token, user, session }) {
+		async jwt({ token, session, user, profile }) {
 			const email = (user ?? token).email!;
-			const dbUser = await prisma.user.findFirst({
+			let dbUser = await prisma.user.findFirst({
 				where: {
 					ot_email: email
 				}
 			});
 			if (dbUser === null) {
-				//TODO: Create user
-				return token;
+				const googleProfile = profile as GoogleProfile;
+				dbUser = await prisma.user.create({
+					data: {
+						ot_email: email,
+						google_id: user.id,
+						role: email.endsWith("@ousd.org")
+							? email.startsWith("s_")
+								? "Student"
+								: "Teacher"
+							: "Community",
+						aeries_first_name: googleProfile.given_name,
+						aeries_last_name: googleProfile.family_name,
+						first_name: googleProfile.given_name,
+						last_name: googleProfile.family_name
+					}
+				});
 			}
 			if (!token.otdata || !dbUser.is_admin) {
 				token.otdata = {
