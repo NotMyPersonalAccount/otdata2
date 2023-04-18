483fcbc7020b18d6903109964ff8e99bee35aa73
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-04-17 23:32:25 -0700

    Animate modal open and close

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -7,7 +7,12 @@ type Props = ReactModal.Props & {
 export default function Modal({ title, ...props }: Props) {
 	return (
 		<ReactModal
-			className="bg-white px-8 py-8 outline-none border-t-2 sm:border-2 border-slate-200 shadow-md rounded-lg w-full sm:w-3/4 fixed sm:top-1/2 sm:left-1/2 bottom-0 sm:bottom-auto right-auto sm:-translate-x-2/4 sm:-translate-y-2/4"
+			className={{
+				base: "modal",
+				afterOpen: "modal-open",
+				beforeClose: "modal-close"
+			}}
+			closeTimeoutMS={200}
 			{...props}
 		>
 			<h1 className="text-3xl font-bold mb-4">{title}</h1>
